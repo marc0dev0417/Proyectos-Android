df5384f04d5020e31ac6f289f4b54ed73cfff3f7
Marquet <marcobenegasdev@outlook.es>
2021-11-10 23:10:48 +0100

    Subo la practica 8 de moviles

---
+++ Practica8/app/src/main/java/com/example/practica8/ActividadBorrar.java
@@ -0,0 +1,13 @@
+package com.example.practica8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActividadBorrar extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
